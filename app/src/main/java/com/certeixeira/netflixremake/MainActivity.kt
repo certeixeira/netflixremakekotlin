3d8706ed8dc79cba8282a6d2474c277c5f5f2821
Carlos Eduardo Rangel Teixeira <certeixeira@outlook.com>
2023-01-26 15:14:42 -0300

    arrumando a lógica do carrossel

--- app/src/main/java/com/certeixeira/netflixremake/MainActivity.kt
+++ app/src/main/java/com/certeixeira/netflixremake/MainActivity.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.certeixeira.netflixremake.model.Category
 import com.certeixeira.netflixremake.model.Movie
 
 class MainActivity : AppCompatActivity() {
@@ -13,15 +14,27 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         Log.i("Teste", "onCreate")
 
+        val categories = mutableListOf<Category>()
+        for (j in 0 until 10) {
+            val movies = mutableListOf<Movie>()
+            for (i in 0 until 15) {
+                val movie = Movie(R.drawable.movie)
+                movies.add(movie)
+            }
+
+            val category = Category("cat $j", movies)
+            categories.add(category)
+        }
+
         val movies = mutableListOf<Movie>()
         for (i in 0 until 60) {
             val movie = Movie(R.drawable.movie)
             movies.add(movie)
         }
 
-        val adapter = MainAdapter(movies)
-        val rv: RecyclerView = findViewById(R.id.rv_main)
-        rv.layoutManager = LinearLayoutManager(this, RecyclerView.HORIZONTAL, false)
+        val adapter = CategoryAdapter(categories)
+        val rv: RecyclerView = findViewById(R.id.rv_category)
+        rv.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
         rv.adapter = adapter
     }
 
