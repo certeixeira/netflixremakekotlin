a1e2ccb8196d538de1325e4a0c044b6e16c80b70
Carlos Eduardo Rangel Teixeira <certeixeira@outlook.com>
2023-03-23 15:06:17 -0300

    adicionando outra forma de ler o json

--- app/src/main/java/com/certeixeira/netflixremake/util/CategoryTask.kt
+++ app/src/main/java/com/certeixeira/netflixremake/util/CategoryTask.kt
@@ -1,7 +1,10 @@
 package com.certeixeira.netflixremake.util
 
 import android.util.Log
+import java.io.BufferedInputStream
+import java.io.ByteArrayOutputStream
 import java.io.IOException
+import java.io.InputStream
 import java.net.URL
 import java.util.concurrent.Executors
 import javax.net.ssl.HttpsURLConnection
@@ -26,17 +29,37 @@ class CategoryTask {
                     throw IOException("Erro na comunicação com o servidor!")
                 }
 
-                //forma 1:
                 val stream = urlConnection.inputStream //sequencia de bytes
-                val jsonAsString = stream.bufferedReader().use { it.readText() }
-                Log.i("Teste", jsonAsString)
+
+                //forma 1:
+//                val jsonAsString = stream.bufferedReader().use { it.readText() }
+
 
                 // forma 2:
+                val buffer = BufferedInputStream(stream)
+                val jasonAsString = toString(buffer)
+                Log.i("Teste", jasonAsString)
 
             } catch (e: java.lang.Exception) {
                 Log.e("Teste", e.message ?: "erro deconhecido", e)
             }
         }
+    }
+
+
+    private fun toString(stream: InputStream): String {
+        val bytes = ByteArray(1024)
+        val baos = ByteArrayOutputStream()
+        var read: Int
+        while (true) {
+            read = stream.read(bytes)
+            if (read <= 0) {
+                break
+            }
+            baos.write(bytes, 0, read)
+        }
+
+        return String(baos.toByteArray())
     }
 
 }
